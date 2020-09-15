751170d236bc883f00538bb86523e0c3be0d0e86
Minakshi Verma <mvedwan@gmail.com>
2020-09-14 20:51:13 -0400

    migration added!

---
+++ migrations/20200317174801_cars.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+  
+    return knex.schema.createTable("cars", tbl=>{
+        tbl.increments();
+        tbl
+            .string("VIN", 20)
+            .notNullable()
+            .unique()
+            .index()
+        tbl
+            .string("Make", 255)
+            .notNullable()
+        tbl
+            .string("Model", 255)
+        tbl
+            .integer("Mileage")
+                    
+      })    
+    };
+    
+    exports.down = function(knex) {
+     return knex.schema.dropTableIfExists("cars")
+      
+    };
